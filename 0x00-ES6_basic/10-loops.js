66b754614cc5b3a59bae83454293323885678890
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-06-24 21:29:22 +0300

    eslint error correction

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
-    const updatedArray = [];
+  const updatedArray = [];
 
-    for (const element of array) {
-        updatedArray.push(appendString + element);
-    }
+  for (const element of array) {
+    updatedArray.push(appendString + element);
+  }
 
-    return updatedArray;
+  return updatedArray;
 }
--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,3 +1,3 @@
 export default function returnHowManyArguments(...inputarguments) {
-    return inputarguments.length;
+  return inputarguments.length;
 }
