2578fd72bd4137634c6009e590b57ae1fe277ca7
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-06-25 13:11:18 +0300

    allSettled promise method

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,23 +1,19 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-// export default function handleProfileSignup(firstName, lastName, fileName) {
-//     return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-//     .then((results) => results.forEach((result) => console.log(result)));
-// }
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const res = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map((result) => {
-      if (result.status === 'fulfilled') {
-        return {
-          status: result.status,
-          value: result.value,
-        };
-      }
+  return res.map((result) => {
+    if (result.status === 'fulfilled') {
       return {
         status: result.status,
-        value: result.reason.toString(),
+        value: result.value,
       };
-    }));
+    }
+    return {
+      status: result.status,
+      value: result.reason.toString(),
+    };
+  });
 }
