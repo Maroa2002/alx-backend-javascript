0e8e30c07252f762f98d285e92d79f8150269150
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-07-03 15:26:52 +0300

    interfaces with functions

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,6 +11,14 @@ interface Directors extends Teacher {
     numberOfReports: number;
 }
 
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+    return `${firstName.charAt(0)}. ${lastName}`;
+};
+
 const teacher3: Teacher = {
     firstName: 'John',
     fullTimeEmployee: false,
@@ -29,4 +37,6 @@ const director1: Directors = {
 
   console.log(director1);
   console.log(teacher3);
+  const print_teacher = printTeacher("John", "Doe");
+  console.log(print_teacher);
   
