db2831749f192802e61f876ce41995374ec53258
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-08-20 14:27:22 +0300

    orgaznizes the structure of the database and creates the funtion to read database

---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,30 @@
+import fs from 'fs/promises';
+
+export default async function readDatabase(path) {
+  try {
+    const data = await fs.readFile(path, 'utf-8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+    if (lines.length === 0) {
+      throw new Error('Cannot load the databse');
+    }
+
+    const fields = {};
+
+    lines.shift(); // removes the header
+
+    lines.forEach((line) => {
+      const [firstname, , , field] = line.split(',');
+
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+
+      fields[field].push(firstname);
+
+      return fields;
+    });
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
