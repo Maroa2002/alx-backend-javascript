084a36765a8c49601877885228489a4e313ba790
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-08-20 12:08:30 +0300

    Create a small HTTP server using Express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const port = 1245;
+const hostname = '127.0.0.1';
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+module.exports = app;
