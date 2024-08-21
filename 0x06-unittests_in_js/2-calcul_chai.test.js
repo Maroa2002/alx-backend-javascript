ba8266ee1efc42274025bfe0569e8c34504bc572
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-08-21 12:31:50 +0300

    Basic test using Chai assertion library

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,26 @@
+const chai = require('chai');
+
+const { expect } = chai;
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+  it('should return 6 when inputs are SUM,1.4 and 4.5', () => {
+    expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+  });
+
+  it('should return -4 when inputs are SUBTRACT, 1.4 and 4.5', () => {
+    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+  });
+
+  it('should return 0.2 when inputs are DIVIDE, 1.4 and 4.5', () => {
+    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.closeTo(0.2, 0.0001);
+  });
+
+  it('should return Error when inputs are DIVIDE, 1.4 and 0', () => {
+    expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+  });
+
+  it('should throw an error for an unknown operation type', () => {
+    expect(() => calculateNumber('MULTIPLY, 1, 2')).to.throw(Error, 'Unknown operation type');
+  });
+});
