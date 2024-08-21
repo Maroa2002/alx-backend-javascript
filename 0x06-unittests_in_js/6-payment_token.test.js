8f114be5ac00305d23acb9ae3e64107af4d5919a
Maroa2002 <mgm.engineeringtie847@gmail.com>
2024-08-21 19:20:26 +0300

    Async tests with done

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -4,12 +4,12 @@ const getPaymentTokenFromAPI = require('./6-payment_token');
 const { expect } = chai;
 
 describe('getPaymentTokenFromAPI', () => {
-  it("should return a resolved promise with {data: 'Successful response from the API' }", () => new Promise((done) => {
+  it("should return a resolved promise with {data: 'Successful response from the API' }", (done) => {
     getPaymentTokenFromAPI(true)
       .then((response) => {
         expect(response).to.include({ data: 'Successful response from the API' });
         done();
       })
       .catch((err) => done(err));
-  }));
+  });
 });
